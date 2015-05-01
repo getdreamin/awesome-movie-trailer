e707fa1d9ee056a3301a3253dcebe9b01739f4aa
Steven Lambert <Steven@getddreamin.com>
2015-04-30 21:23:06 -0700

    Added Initial Files

---
+++ media.py
@@ -0,0 +1,28 @@
+import webbrowser
+
+class Movie():
+    """Creates a movie with the necessary pieces of information
+       about that movie
+
+    Attributes:
+        title: A string for the title of the movie
+        storyline: A string for the summary of the movie
+        poster_image_url: A url for the poster image
+        trailer_youtube_url: A url to watch the trailer
+        release_date: The release date of the movie
+        """
+    
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                     trailer_youtube, release_date):
+            
+       self.title = movie_title
+       self.storyline = movie_storyline
+       self.poster_image_url = poster_image
+       self.trailer_youtube_url = trailer_youtube
+       self.release_date = release_date
+
+        
+    def show_trailer(self):
+            #Used to display the trailer of the movie
+            #object passed to it in a web browser
+            webbrowser.open(self.trailer_youtube_url)
