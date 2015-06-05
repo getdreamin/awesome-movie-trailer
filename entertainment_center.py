37eaf333502c6cb5d4e32ac0f2068d556c31eeb6
Steven Lambert <Steven@getddreamin.com>
2015-06-05 01:22:18 -0700

    Updated for formatting issues

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,43 +1,56 @@
 import fresh_tomatoes
 import media
 
-#Creating each of the movie objects with their associated information
+# Creating each of the movie objects with their associated information
 tucker = media.Movie("Tucker", "The story of a man and his dreams",
-                     "http://upload.wikimedia.org/wikipedia/en/2/28/Tuckerposter.jpg",
+                     "http://upload.wikimedia.org/wikipedia/en/2/28",
+                     "/Tuckerposter.jpg",
                      "https://www.youtube.com/watch?v=ty93RYkzYQQ",
                      "August 12, 1988")
 
-avengers = media.Movie("The Avengers", "Earth's mightiest heroes defend earth",
-                       "http://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
+avengers = media.Movie("The Avengers", "Earth's mightiest heroes defend",
+                       "earth",
+                       "http://upload.wikimedia.org/wikipedia/en/f/f9/",
+                       "TheAvengers2012Poster.jpg",
                        "https://www.youtube.com/watch?v=eOrNdBpGMv8",
                        "May 4, 2012 (USA)")
 
 imitation = media.Movie("The Imitation Game",
-                        "Alan Turing works to crack the code of the Enigma Machine",
-                        "http://upload.wikimedia.org/wikipedia/en/5/5e/The_Imitation_Game_poster.jpg",
+                        "Alan Turing works to crack the code of the",
+                        " Enigma Machine",
+                        "http://upload.wikimedia.org/wikipedia/en/5/5e",
+                        "/The_Imitation_Game_poster.jpg",
                         "https://www.youtube.com/watch?v=S5CjKEFb-sM",
                         "January 7, 2015 (Egypt)")
 
 transcendence = media.Movie("Transendence",
-                            "An AI researcher achieves transcendence by going digital",
-                            "http://upload.wikimedia.org/wikipedia/en/e/ef/Transcendence2014Poster.jpg",
+                            "An AI researcher achieves transcendence",
+                            "by going digital",
+                            "http://upload.wikimedia.org/wikipedia/en/e/ef",
+                            "/Transcendence2014Poster.jpg",
                             "https://www.youtube.com/watch?v=VCTen3-B8GU",
                             "April 10, 2014 (USA)")
 
 holy_grail = media.Movie("Monty Python and the Quest for the Holy Grail",
-                         "A group of knights seek out the Holy Grail, hilarity ensues",
-                         "http://upload.wikimedia.org/wikipedia/en/4/49/Monty_python_and_the_holy_grail_2001_release_movie_poster.jpg",
+                         "A group of knights seek out the Holy Grail, ",
+                         "hilarity ensues",
+                         "http://upload.wikimedia.org/wikipedia/en/4/49/",
+                         "Monty_python_and_the_holy_grail_2001_release_",
+                         "movie_poster.jpg",
                          "https://www.youtube.com/watch?v=urRkGvhXc8w",
                          "January 1, 1975 (USA)")
 
 to_the_greek = media.Movie("Get Him to the Greek",
-                           "An understudy of Jay-Z has to get a washed up rock star to play a show",
-                           "http://upload.wikimedia.org/wikipedia/en/c/c2/Get_Him_to_the_Greek.jpg",
+                           "An understudy of Jay-Z has to get a washed ",
+                           "up rock star to play a show",
+                           "http://upload.wikimedia.org/wikipedia/en/c/c2",
+                           "/Get_Him_to_the_Greek.jpg",
                            "https://www.youtube.com/watch?v=N6ixkr0-qvo",
                            "June 4, 2010 (USA)")
 
-#The array of movies to be loaded
-movies = [tucker, avengers, imitation, transcendence, holy_grail, to_the_greek]
+# The array of movies to be loaded
+movies = [tucker, avengers, imitation, transcendence,
+          holy_grail, to_the_greek]
 
-#Takes the array of movies and displays them on a webpage
+# Takes the array of movies and displays them on a webpage
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+
 class Movie():
     """Creates a movie with the necessary pieces of information
        about that movie
@@ -11,18 +12,16 @@ class Movie():
         trailer_youtube_url: A url to watch the trailer
         release_date: The release date of the movie
         """
-    
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                     trailer_youtube, release_date):
-            
-       self.title = movie_title
-       self.storyline = movie_storyline
-       self.poster_image_url = poster_image
-       self.trailer_youtube_url = trailer_youtube
-       self.release_date = release_date
 
-        
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube, release_date):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.release_date = release_date
+
+    # Used to display the trailer of the movie
+    # object passed to it in a web browser
     def show_trailer(self):
-            #Used to display the trailer of the movie
-            #object passed to it in a web browser
             webbrowser.open(self.trailer_youtube_url)
